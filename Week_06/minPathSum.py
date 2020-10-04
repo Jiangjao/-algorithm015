4070bfa89bb358b52472039a75af1d9d876b8aaf
jayjiao <918718278@qq.com>
2020-10-04 20:38:16 +0800

    honework02

---
+++ Week_06/minPathSum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        raws = len(grid)
+        cols = len(grid[0])
+        # 初始化dp数组
+        dp = [[0]*cols for _ in range(raws)]
+        dp[0][0] = grid[0][0]
+        for raw in range(raws):
+            for col in range(cols):
+                if col == 0 and raw == 0:
+                    continue
+                if raw == 0:
+                    dp[raw][col] = dp[raw][col-1] + grid[raw][col]
+                elif col == 0:
+                    dp[raw][col] = dp[raw-1][col] + grid[raw][col]
+                else:
+                    dp[raw][col] = min(dp[raw-1][col],dp[raw][col-1]) + grid[raw][col]
+        
+        return dp[raws-1][cols-1]
